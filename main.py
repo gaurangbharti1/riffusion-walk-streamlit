50c0149291d091c6f710da399976a565b175b40f
Gaurang Bharti <gaurangbharti@gmail.com>
2023-05-03 02:38:16 -0700

    Main commit

--- main.py
+++ main.py
@@ -2,12 +2,11 @@ import streamlit as st
 import requests
 import time
 
-API_KEY = str(st.secrets["SIEVE_API_KEY"])
+API_KEY = str(st.secrets['SIEVE_API_KEY'])
 
-st.title("Stable Riffusion Walk")
+st.title("Stable Diffusion Music Video")
 st.markdown('Built by [Gaurang Bharti](https://twitter.com/gaurang_bharti) powered by [Sieve](https://www.sievedata.com)')
-st.markdown("This web app uses a workflow that combines [Stable Riffusion](https://github.com/riffusion/riffusion) with [Stable Diffusion Walk](https://github.com/nateraw/stable-diffusion-videos)")
-st.write("This workflow generates music and a video based on the input prompt that is then combined")
+st.markdown("This web app allows you to create your own Music Video using Stable Diffusion!")
 
 def check_status(url, interval, job_id):
     finished = False
@@ -16,7 +15,9 @@ def check_status(url, interval, job_id):
         }
     while True:
         response = requests.get(url, headers=headers)
+        assert response.json()['data'], print(response.json())
         data = response.json()['data']
+        
         for job in data:
             if job['id'] == job_id:
             
@@ -43,7 +44,7 @@ def fetch_video(job_id):
     url = data['data'][0]['url']
     return url
 
-def send_data(text, duration, name):
+def send_data(audio_text, video_text, duration, name):
     url = "https://mango.sievedata.com/v1/push"
     
     headers = {
@@ -53,7 +54,8 @@ def send_data(text, duration, name):
     data = {
         "workflow_name": name,
         "inputs": {
-            "prompt": text,
+            "video_prompt": video_text,
+            "audio_prompt": audio_text,
             "duration": duration
             }
         }
@@ -67,19 +69,44 @@ def send_data(text, duration, name):
         return (f'An error occurred: {e}')
     
 #Streamlit App
+st.subheader("Music")
+audio_in = st.text_input('What kind of music would you like?', max_chars=100)
 
-text_in = st.text_input('Enter a prompt describing an audiovisual', max_chars=100)
+music_options = ["None", "Guitar riff", "Jazz Solo", "new orleans blues", "Berlin and Paris electronic fused together with acoustic tunes"]
+music_example = st.radio("Or pick one of these!", music_options)
+
+if music_example != "None":
+    audio_in = music_example
+
+st.subheader("Video")
+
+video_in = st.text_input("Describe the visuals of the video", max_chars=100)
+
 input_duration = st.slider("Duration (seconds)", 4, 7, 5)
-workflow_name = "stable-riffusion-walk"
 
-button1 = st.button("Riffusion Walk")
+st.write("Pick one of these models for the video")
+options = ["Openjourney", "Stable Diffusion v1.5"]
+col1, col2 = st.columns([1, 1])
+with col1:
+    selected_option = st.radio("Select an option", options)
+with col2:
+    if selected_option == "Stable Diffusion v1.5":
+        st.image("sd_21_2.jpg", width=300)
+        workflow_name = "stable-riffusion-walk"
+    elif selected_option == "Openjourney":
+        st.image("openjourney_1.png", width=300)
+        workflow_name = "openjourney-test"
+    
+#workflow_names = ["stable-riffusion-walk", "openjourney-test"]
+
+button1 = st.button("Generate Music Video")
 
 if st.session_state.get('button') != True:
     st.session_state['button'] = button1
 
 if st.session_state['button'] == True:
 
-    job = send_data(text_in, input_duration, workflow_name)
+    job = send_data(audio_in, video_in, input_duration, workflow_name)
     if job:
         with st.spinner("Processing Video"):
             status = check_status('https://mango.sievedata.com/v1/jobs', 5, str(job))
